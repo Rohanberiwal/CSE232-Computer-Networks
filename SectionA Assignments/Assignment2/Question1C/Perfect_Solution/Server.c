19517e67ecbaaa43f2fc87aa3f332dbe31986775
Aristotle of Stagira <119040957+Rohanberiwal@users.noreply.github.com>
2024-09-25 11:00:45 +0530

    Update Server.c
    This is  the most perfect client server code for the TCP connection .

--- SectionA Assignments/Assignment2/Question1C/Perfect_Solution/Server.c
+++ SectionA Assignments/Assignment2/Question1C/Perfect_Solution/Server.c
@@ -9,33 +9,38 @@
 #define PORT 8081
 #define MAX_EVENTS 100
 #define BUFFER_SIZE 2048
+#define MAX_CONNECTIONS 10
 
-void handle_error(const char *error_out) {
+void handle_error(const char *error_out) 
+{
     printf(error_out);
     exit(EXIT_FAILURE);
 }
 
-void cleanup(int server_fd, int epfd) {
-    if (server_fd != -1) {
+void cleanup(int server_fd, int epfd) 
+{
+    if (server_fd != -1) 
+    {
         close(server_fd);
     }
-    if (epfd != -1) {
+    if (epfd != -1) 
+    {
         close(epfd);
     }
 }
 
-
-void handle_client(int client_fd) {
+void handle_client(int client_fd) 
+{
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-    
-    if (bytes_read < 0) {
-        perror("Read failed");
+    if (bytes_read < 0) 
+    {
+        printf("Read command failed");
         close(client_fd);
     }
     else if (bytes_read == 0) 
     {
-        printf("Closing connection on fd %d\n", client_fd);
+        printf("Closing connection for file descriptor %d\n", client_fd);
         close(client_fd);
     } 
     else 
@@ -43,69 +48,91 @@ void handle_client(int client_fd) {
         buffer[bytes_read] = '\0';
         printf("Received message: %s\n", buffer);
         const char *response = "Hello from the server\n";
-        if (send(client_fd, response, strlen(response), 0) == -1) {
-            perror("Send failed");
+        if (send(client_fd, response, strlen(response), 0) == -1) 
+        {
+            printf("Send  command failed");
             close(client_fd);
         }
     }
 }
 
-int main() {
-    int server_fd = -1, epfd = -1;
+
+int main() 
+{
+
     struct sockaddr_in server_addr;
     struct epoll_event event, events[MAX_EVENTS];
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    int epfd = -1;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+    //printf("%d",server_fd) ;
+    if (server_fd == -1) 
+    {
         handle_error("Socket creation failed");
     }
     
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        cleanup(server_fd, epfd);
-        handle_error("Bind operation failed");
-    }
-
-    if (listen(server_fd, 10) == -1) 
+    int bind_output =  bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ;
+    //printf("%d",bind_output) ;
+    if (bind_output == -1) 
     {
         cleanup(server_fd, epfd);
-        handle_error("Listen command failed");
+        handle_error("Bind operation failed cleanup completed");
     }
-
-    if ((epfd = epoll_create1(0)) == -1) 
+    int listen_output =  listen(server_fd, MAX_CONNECTIONS) ;
+    //  printf("%d",listen_output) ;
+    if (listen_output == -1) 
     {
         cleanup(server_fd, epfd);
-        handle_error("epoll_create1 failed");
+        handle_error("Listen command failed  , cleanup completed");
     }
 
-    // Add the server socket to the epoll instance
-    event.events = EPOLLIN; // Watch for input events
-    event.data.fd = server_fd; // Use the server_fd as the user data
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+    epfd = epoll_create1(0) ;
+    if (epfd == -1) 
+    {
+        cleanup(server_fd, epfd);
+        handle_error("epoll_create1   function call failed");
+    }
+
+    event.events = EPOLLIN; 
+    event.data.fd = server_fd; 
+    int event_status = epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &event) ;
+    //  printf("%d",event_status) ;
+    //  printf("%d",event.data.fd) ;
+    if (event_status == -1) 
+    {
         cleanup(server_fd, epfd);
         handle_error("epoll_ctl failed for server_fd");
     }
+    else 
+    {
+        printf("The socket event is registered wihtout any issue ") ;
+        printf("\n") ;
+        printf("Server listening on port %d\n", PORT);
+    }
 
-    printf("Server listening on port %d\n", PORT);
-
-    while (1) {
+    while (1) 
+    {
         int num_fds = epoll_wait(epfd, events, MAX_EVENTS, -1);
-        if (num_fds == -1) {
+        if (num_fds == -1) 
+        {
             cleanup(server_fd, epfd);
-            handle_error("epoll_wait failed");
+            handle_error("epoll_wait  fucntion failed , cleanup handled properly");
         }
 
-        for (int i = 0; i < num_fds; i++) {
-            if (events[i].data.fd == server_fd) {
-                // Accept a new client connection
+        for (int i = 0; i < num_fds; i++) 
+        {
+            if (events[i].data.fd == server_fd) 
+            {
                 int client_fd = accept(server_fd, NULL, NULL);
-                if (client_fd == -1) {
-                    perror("Accept operation failed");
-                    continue; // Handle error but continue accepting connections
-                }
-                event.events = EPOLLIN; // Watch for input events
+                event.events = EPOLLIN; 
                 event.data.fd = client_fd; 
+                if (client_fd == -1)
+                {
+                    printf("Accept function failed");
+                    continue;
+                }
                 if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &event) == -1) 
                 {
                     printf("epoll_ctl failed for client_fd");
@@ -113,7 +140,7 @@ int main() {
                     continue;
                 }
 
-                printf("Accepted new connection on fd %d\n", client_fd);
+                printf("Recived Newe on file descriptor number %d\n", client_fd);
             } 
             else 
             {
@@ -125,3 +152,4 @@ int main() {
     cleanup(server_fd, epfd);
     return 0;
 }
+
