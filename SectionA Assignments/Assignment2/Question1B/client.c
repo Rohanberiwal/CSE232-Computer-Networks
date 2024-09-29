583b4a1354be32f11fc5c97e7ea8fc09d9926e58
Aristotle of Stagira <119040957+Rohanberiwal@users.noreply.github.com>
2024-09-29 20:45:37 +0530

    Update client.c
    This is hte  updated Code for  the client . this is taken from, the aks repo .

--- SectionA Assignments/Assignment2/Question1B/client.c
+++ SectionA Assignments/Assignment2/Question1B/client.c
@@ -1,79 +1,81 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 8081
 #define BUFFER_SIZE 2048
 
-int main() {
+void *client_task(void *arg) {
     int sock;
     struct sockaddr_in server_addr;
-    const char *message = "Hello from client file ";
+    const char *message = "Hello from client";
     char buffer[BUFFER_SIZE] = {0};
 
+    // Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Socket Failed ");
-        exit(EXIT_FAILURE);
+        perror("Socket creation failed");
+        return NULL;
     }
 
-    // sin port  for the port number used by the user . 
-    // sin_family is for the struct of the IPV4 and deifnes the socket and realted .
-    // AF_Inet is used for the ipv 4 and AF_Inet_6 is used fot eh IP v6
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
 
-    
-    // inet_pton is use for the conversion of the IP from the textual format to the Bin format 
-    // initally the Ip is in 127.0.0.1 string fromat after using this we can get this as a bin format 
-    int binary_format_Ip  = inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) ;
-    //printf("Bin format is " , binary_format_Ip) ;
-    if (binary_format_Ip <= 0) {
-        printf("The IP address is invalid") ;
+    // Convert IP address
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        printf("Invalid address");
         close(sock);
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
-    // connect() use to connet the Cleint and the server  ; returns a  integer value 
-    int connection_status = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) ;
-    if (connection_status < 0) {
-        printf("Connection failed , there could be seevral issue ");
-        printf("\n") ;
-        printf("Server might not be initaited ") ;
-        printf("\n") ;
-        printf("Socket descriptor might be failed ") ;
-        printf("\n") ;
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Connection failed");
         close(sock);
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
-    // send() is use to check the number of the bytes send after the connect() is estabilshed 
+    // Send message
     ssize_t bytes_sent = send(sock, message, strlen(message), 0);
-    if (bytes_sent < 0) 
-    {
-        printf("Zero byte are send , there is a issue ");
+    if (bytes_sent < 0) {
+        printf("Send failed");
         close(sock);
-        exit(EXIT_FAILURE);
-    }
-    else 
-    {
-    printf("Message sent to server\n");
+        return NULL;
     }
 
-    // read is use to get the output or the messge from the server.c 
-    // if the read is zero then there is a error 
+    // Read response
     ssize_t bytes_read = read(sock, buffer, sizeof(buffer) - 1);
     if (bytes_read < 0) {
         printf("Read failed");
         close(sock);
-        exit(EXIT_FAILURE);
+        return NULL;
+    } else if (bytes_read == 0) {
+        printf("Server disconnected unexpectedly\n");
+        close(sock);
+        return NULL;
     }
 
-    // this part is handling the null character from the bytes recived in the buffer from server.c
-    buffer[bytes_read] = '\0';
+    buffer[bytes_read] = '\0';  // Null-terminate the buffer
     printf("Server response: %s\n", buffer);
 
     close(sock);
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    // Create threads
+    pthread_create(&t1, NULL, client_task, NULL);
+    pthread_create(&t2, NULL, client_task, NULL);
+
+    // Wait for threads to finish
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
     return 0;
 }
+
