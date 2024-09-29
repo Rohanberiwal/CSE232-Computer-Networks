98395f8c4dafab755cc98de10eac41a3da0cedaa
Aristotle of Stagira <119040957+Rohanberiwal@users.noreply.github.com>
2024-09-29 20:41:42 +0530

    Update Server.c
    This is akansha singal cpp -> c code

--- SectionA Assignments/Assignment2/Question1B/Server.c
+++ SectionA Assignments/Assignment2/Question1B/Server.c
@@ -11,14 +11,14 @@
 #define PORT 8081
 #define BUFFER_SIZE 2048
 
+
 int count_user  = 0 ;
 
-void *handle_client(void *client_socket) {
-    int sock = *(int *)client_socket;
-    char buffer[BUFFER_SIZE] = {0};
-    const char *response = "Hello from server file";
-
-    // PID to print the thread Id that is runnign as this is a code for multi threading 
+void *handle_client(void *arg) {
+    int new_socket = *(int *)arg;
+    free(arg); // Free the allocated memory for the socket
+    char buffer[1024] = {0};
+    const char *hello = "Hello from server";
 
     pid_t tid = syscall(SYS_gettid);
     count_user = count_user + 1;
@@ -26,98 +26,83 @@ void *handle_client(void *client_socket) {
     printf("\n") ;
     printf("Thread PID(process id on the CPU): %d, Thread ID: %ld\n", getpid(), tid);
     printf("\n") ;
-    // Read the buffer bytes received (remove 1 because of the null character)
-    ssize_t bytes_read = read(sock, buffer, sizeof(buffer) - 1);
-    if (bytes_read < 0) {
-        printf("Read operation failed\n");
-        close(sock);
-        pthread_exit(NULL);
-    }
 
-    // Null-terminate the buffer
-    buffer[bytes_read] = '\0';
+    ssize_t bytes_read = read(new_socket, buffer, sizeof(buffer));
+    if (bytes_read < 0) {
+        perror("Read error");
+        close(new_socket);
+        return NULL;
+    } else if (bytes_read == 0) {
+        printf("Client disconnected unexpectedly\n");
+        close(new_socket);
+        return NULL;
+    }
+    
     printf("Message received: %s\n", buffer);
 
-    // Send response to the client
-    ssize_t bytes_sent = send(sock, response, strlen(response), 0);
+    ssize_t bytes_sent = send(new_socket, hello, strlen(hello), 0);
     if (bytes_sent < 0) {
-        printf("Send failed\n");
-        close(sock);
-        pthread_exit(NULL);
+        perror("Send error");
+        close(new_socket);
+        return NULL;
     }
 
-    printf("Response successfully sent to client\n");
-
-    close(sock);
-    pthread_exit(NULL);
+    printf("Hello message sent\n");
+    close(new_socket);
+    return NULL;
 }
 
 
-int main() 
-{
-    // insttantiae the server file descriptor to listne the ports 
-    // make  the client socket that is  use to recive the messager from the client
-
-    int server_fd, client_socket;
-    //  the socket_in  has the server_address and teh port number .
-    struct sockaddr_in server_addr, client_addr;
-
-    socklen_t client_len = sizeof(client_addr);
-    pthread_t thread_id;
+int main() {
+    int server_fd;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
+        printf("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // There is a additional sin_add.s_addr this is use to specify which socket must  the server bidn to . 
-    // The rest of the add are the smae  , teh family is the IPV4 and the sin_port is the port no
-    
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8081); // Changed to 8081
 
-    // do the bind operation 
-    // bind does the binding to teh port mentioned here is
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        printf("Bind operation  failed");
-        close(server_fd);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        printf("Bind failed");
         exit(EXIT_FAILURE);
     }
-    int max_queue_user = 10 ;
-    // Listen to the cleint after the bind is done 
-    if (listen(server_fd, max_queue_user) < 0) {
-        printf("Server listen command failed ");
-        close(server_fd);
+
+    if (listen(server_fd, 3) < 0) {
+        printf("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d\n", PORT);
 
-    // Use a while loop where we accpe the message from the cleint scoket to the server side .
-    //  this is done after the establishment of teh TCP from the listena and bind()
     while (1) {
-        // This is use to accpet the message from the client 
-        client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-        if (client_socket < 0) {
-            printf("Accept operation failed");
+        int *new_socket = malloc(sizeof(int));
+        if (new_socket == NULL) {
+            perror("Failed to allocate memory");
+            exit(EXIT_FAILURE);
+        }
+
+        if ((*new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            printf("Accept failed");
+            free(new_socket); // Free memory if accept fails
             continue;
         }
 
-        // this is the concurrent function thta is handling  the cliennt handle operation 
-        if (pthread_create(&thread_id, NULL, handle_client, (void *)&client_socket) != 0) 
-        {
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, handle_client, new_socket) != 0) {
             printf("Thread creation failed");
-            close(client_socket);
-        } 
-
-        else 
-        {
-            pthread_detach(thread_id);
+            close(*new_socket);
+            free(new_socket);
+        } else {
+            pthread_detach(thread_id); // Detach the thread to clean up resources automatically
         }
     }
 
     close(server_fd);
     return 0;
 }
+
